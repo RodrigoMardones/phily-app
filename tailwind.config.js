1d5f5c09e8295893aa1146557d054ec61ac23abc
Rodrigo Mardones <rodrigo.mardones.a@usach.cl>
2024-04-17 00:17:14 -0400

    mod: update ux/ui component lib

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#004cff',
+          secondary: '#00bb00',
+          accent: '#00a4d1',
+          neutral: '#040404',
+          'base-100': '#ffffe3',
+          info: '#00a0ff',
+          success: '#87c900',
+          warning: '#cb5200',
+          error: '#df002a',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+}
