5107293bbf384de680239e1f7a0d43704174c6be
emeitiai <matinunezmora@gmail.com>
2024-06-05 00:04:51 -0400

    refactor: add hook for upload files

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,97 +2,18 @@ import { forwardRef } from 'react';
 import Image from 'next/image';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Card } from 'react-daisyui';
-import { parseStringToTree } from '@/utils/TreeData';
-import { set, getTree, RESET as resetTree } from '../store/tree/slice';
-import { setError, getError, RESET as resetError } from '../store/error/slice';
-import { getFile, setFile, RESET as resetFile } from '../store/file/slice';
+
 import Error from '../error/error';
 import UploadIcon from '../icons/upload';
 import DeleteIcon from '../icons/delete';
 import useDownload from './hooks/useDownload';
+import useUploadFile from './hooks/useUploadFile';
 const accepts = ['.nwk', '.json'];
 
 const Dashboard = (props, ref) => {
 
   const { handleDownload, handleChangeSelectDownload } = useDownload()
-
-  let fileReader;
-  const dispatch = useDispatch();
-  const tree = useSelector(getTree);
-  const file = useSelector(getFile);
-  const error = useSelector(getError);
-
-  const handleFileRead = () => {
-    const extension = fileReader.name.split('.').pop();
-    dispatch(
-      setFile({
-        name: fileReader.name,
-        content: fileReader.result,
-        extension: extension,
-      })
-    );
-  };
-  const handleFileOnChange = (e) => {
-    const files = e.target.files;
-    if (files?.length) {
-      fileReader = new FileReader();
-      fileReader.name = files[0].name;
-      fileReader.onloadend = handleFileRead;
-      fileReader.readAsText(files[0]);
-    }
-  };
-  const handleLoadClick = async (e) => {
-    /** USECASES */
-    /**
-     * 1. No file selected
-     */
-    e.preventDefault();
-    if (!file.name) {
-      dispatch(
-        setError({
-          message: 'No se ha seleccionado un archivo',
-          open: true,
-        })
-      );
-      return;
-    }
-    /**
-     * 2. File loaded twice
-     */
-    if (file.name === tree.name) {
-      dispatch(
-        setError({
-          message: 'Se ha cargado el mismo archivo',
-          open: true,
-        })
-      );
-      return;
-    }
-    /**
-     * 3. File loaded first time
-     */
-    // validar format antes de cargar completo
-    if (file.name !== tree.name) {
-      if (file.extension == 'json') {
-        console.log(file.content);
-        dispatch(
-          set({ ...tree, tree: JSON.parse(file.content), name: file.name })
-        );
-      }
-      if (file.extension == 'nwk') {
-        const parsedTree = parseStringToTree(file.content);
-        dispatch(set({ ...tree, tree: parsedTree, name: file.name }));
-      }
-    }
-  };
-  const handleCleanClick = (e) => {
-    e.preventDefault();
-    dispatch(resetFile());
-    dispatch(resetTree());
-    dispatch(resetError());
-    document.getElementById('normalize').checked = false;
-    document.getElementById('angle').value = 360;
-  };
+  const { tree, error, file, handleCleanClick, handleFileOnChange, handleLoadClick } = useUploadFile()
 
   return (
     <>
--- src/components/dashboard/hooks/useUploadFile.js
+++ src/components/dashboard/hooks/useUploadFile.js
@@ -1,7 +1,92 @@
 import React from 'react'
+import { parseStringToTree } from '@/utils/TreeData';
+import { set, getTree, RESET as resetTree } from '../../store/tree/slice';
+import { setError, getError, RESET as resetError } from '../../store/error/slice';
+import { getFile, setFile, RESET as resetFile } from '../../store/file/slice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const useUploadFile = () => {
-  return {}
+
+  let fileReader;
+  const dispatch = useDispatch();
+  const tree = useSelector(getTree);
+  const file = useSelector(getFile);
+  const error = useSelector(getError);
+
+  const handleFileRead = () => {
+    const extension = fileReader.name.split('.').pop();
+    dispatch(
+      setFile({
+        name: fileReader.name,
+        content: fileReader.result,
+        extension: extension,
+      })
+    );
+  };
+  const handleFileOnChange = (e) => {
+    const files = e.target.files;
+    if (files?.length) {
+      fileReader = new FileReader();
+      fileReader.name = files[0].name;
+      fileReader.onloadend = handleFileRead;
+      fileReader.readAsText(files[0]);
+    }
+  };
+  const handleLoadClick = async (e) => {
+    /** USECASES */
+    /**
+     * 1. No file selected
+     */
+    e.preventDefault();
+    if (!file.name) {
+      dispatch(
+        setError({
+          message: 'No se ha seleccionado un archivo',
+          open: true,
+        })
+      );
+      return;
+    }
+    /**
+     * 2. File loaded twice
+     */
+    if (file.name === tree.name) {
+      dispatch(
+        setError({
+          message: 'Se ha cargado el mismo archivo',
+          open: true,
+        })
+      );
+      return;
+    }
+    /**
+     * 3. File loaded first time
+     */
+    // validar format antes de cargar completo
+    if (file.name !== tree.name) {
+      if (file.extension == 'json') {
+        console.log(file.content);
+        dispatch(
+          set({ ...tree, tree: JSON.parse(file.content), name: file.name })
+        );
+      }
+      if (file.extension == 'nwk') {
+        const parsedTree = parseStringToTree(file.content);
+        dispatch(set({ ...tree, tree: parsedTree, name: file.name }));
+      }
+    }
+  };
+  const handleCleanClick = (e) => {
+    e.preventDefault();
+    dispatch(resetFile());
+    dispatch(resetTree());
+    dispatch(resetError());
+    document.getElementById('normalize').checked = false;
+    document.getElementById('angle').value = 360;
+  };
+
+
+  return { error, tree, file, handleCleanClick, handleLoadClick, handleFileOnChange}
 }
 
 export default useUploadFile
