0b51c54b31a120cc55c9caeda80501301e34bb9b
Rodrigo Mardones <rodrigo.mardones.a@usach.cl>
2024-10-04 00:04:55 -0300

    mod: update color download icon

--- src/components/icons/download.js
+++ src/components/icons/download.js
@@ -7,6 +7,7 @@ const DownloadIcon = ({ className }) => {
     height="24"
     viewBox="0 0 24 24"
     fill="none"
+    className={className}
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
