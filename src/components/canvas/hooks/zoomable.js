d01708f9a709119603cc44024f06343f1a0a4f46
Rodrigo Mardones Aguilar <rodrigo.mardones.a@usach.cl>
2024-10-11 16:15:55 -0300

    mod: fix error onClick not working on svg when contextMenu is not used (#18)

--- src/components/canvas/hooks/zoomable.js
+++ src/components/canvas/hooks/zoomable.js
@@ -1,30 +1,27 @@
 import * as d3 from 'd3';
 import { useRef } from 'react';
 import { useState, useEffect } from 'react';
-import { handleClose } from '../../submenu/useSubmenu';
+import useSubMenu from '../../submenu/useSubmenu';
 
-export default function ZoomableSVG({ children, width, height}) {
+export default function ZoomableSVG({ children, width, height }) {
   const svgRef = useRef();
   const [k, setK] = useState(1);
   const [x, setX] = useState(width);
   const [y, setY] = useState(height);
+  const { handleClose } = useSubMenu();
 
   useEffect(() => {
-    const zoom = d3.zoom()
-    .on('zoom', (event) => {
+    const zoom = d3.zoom().on('zoom', (event) => {
       const { x, y, k } = event.transform;
       setX(x);
       setY(y);
       setK(k);
-    })
+    });
     d3.select(svgRef.current).call(zoom);
   }, []);
   return (
-      <svg ref={svgRef} width={"100%"} height={"100%"}>
-        <g
-          transform={`translate(${x},${y})scale(${k})`}
-          onClick={handleClose}
-          >{children}</g>
-      </svg>
+    <svg ref={svgRef} width={'100%'} height={'100%'} onClick={handleClose}>
+      <g transform={`translate(${x},${y})scale(${k})`}>{children}</g>
+    </svg>
   );
 }
