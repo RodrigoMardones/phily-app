59f71ce09cb5d9024c868a20bf512e98edfc990a
Rodrigo Mardones <rodrigo.mardones.a@usach.cl>
2025-01-04 18:47:32 -0300

    mod: format api

--- src/pages/api/toJson.js
+++ src/pages/api/toJson.js
@@ -2,17 +2,17 @@ import { parseStringToTree, createTreeState } from '../../lib/TreeData';
 
 // todo: agregar validaciones de seguridad
 export default async function handler(req, res) {
-    if(req.method === 'POST') {
-        try {
-            const newickFile = req.body;
-            console.log(newickFile);
-            const pasedTree = createTreeState({
-                name: 'DendrogramToJsonApi',
-                tree: parseStringToTree(newickFile),
-            });
-            return res.status(200).json(pasedTree);
-        } catch(e) {
-            return res.status(500).json({message: e.message});
-        }	
+  try {
+    if (req.method === 'POST') {
+      const newickFile = req.body;
+      console.log(newickFile);
+      const pasedTree = createTreeState({
+        name: 'DendrogramToJsonApi',
+        tree: parseStringToTree(newickFile),
+      });
+      return res.status(200).json(pasedTree);
     }
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
 }
