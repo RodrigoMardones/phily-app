87740323baae98a19acb660e2cd4f83f410f60d8
Rodrigo Mardones <rodrigo.mardones.a@usach.cl>
2025-01-09 00:17:16 -0300

    mod: added extra config to server nginx.config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(www.phily.cl phily.cl)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="rodrigo.mardones.a@usach.cl" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
